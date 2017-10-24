bc8b558ca6fef3de5f02ea9ce60a33094af8bb5b
a1liz <694426737@qq.com>
2017-10-24 22:40:13 +0800

    完成Server和Client的监听部分

--- src/com/liz/Client.java
+++ src/com/liz/Client.java
@@ -2,6 +2,12 @@ package com.liz;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by liz on 17/10/24.
@@ -21,7 +27,6 @@ public class Client extends JFrame {
     private JButton getDateButton;
     private JButton getDirButton;
     private JButton sendButton;
-    private JComboBox sendComboBox;
     private JPanel receivePanel;
     private JEditorPane receiveEditorPane;
     private JLabel currLabel;
@@ -36,6 +41,63 @@ public class Client extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
+    }
+
+    private void sendPacket(int type) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        byte[] buf = new byte[256];
+        String contents;
+        if (type < 1 || type > 4) {
+            receiveEditorPane.setText(receiveEditorPane.getText() + "function sendPacket type error!\n");
+            return;
+        }
+        contents = String.valueOf(type) + "|";
+        String notice = "";
+        switch (type) {
+            case 1:
+                notice = "发送获取时间请求";
+                break;
+            case 2:
+                notice = "发送获取日期请求";
+                break;
+            case 3:
+//                if (sendEditorPane.getText().equals("")) {
+//                    notice = "发送获取当前目录请求";
+//                    contents += ".|";
+//                    break;
+//                }
+//                notice = "发送获取" + sendEditorPane.getText() + "目录请求";
+//                contents += sendEditorPane.getText() + "|";
+                notice = "发送获取当前目录请求";
+                contents += ".|";
+                break;
+            case 4:
+                notice = "发送信息请求";
+                contents += sendEditorPane.getText() + "|";
+                break;
+        }
+
+
+        buf = contents.getBytes();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(urlField.getText()), Integer.parseInt(portField.getText()));
+        socket.send(packet);
+
+        buf = new byte[256];
+        packet = new DatagramPacket(buf,buf.length);
+        try {
+            socket.setSoTimeout(500);
+            socket.receive(packet);
+        } catch (SocketTimeoutException e) {
+            currLabel.setText("连接服务器失败,请重试");
+            receiveEditorPane.setText(receiveEditorPane.getText() + "连接服务器失败,请重试\n");
+            socket.close();
+            return;
+        }
+        receiveEditorPane.setText(receiveEditorPane.getText() + notice +"成功\n");
+        currLabel.setText("连接到服务器"+urlField.getText()+":"+portField.getText()+"成功!");
+        String received = new String(packet.getData());
+        receiveEditorPane.setText(receiveEditorPane.getText() + "接收到: " + received + "\n");
+        socket.close();
     }
 
     private void addPanel() {
@@ -75,12 +137,52 @@ public class Client extends JFrame {
         choosePanel.setLayout(new FlowLayout());
         getTimeButton = new JButton();
         getTimeButton.setText("获取时间");
+        getTimeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    sendPacket(1);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
         getDateButton = new JButton();
         getDateButton.setText("获取日期");
+        getDateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    sendPacket(2);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
         getDirButton = new JButton();
-        getDirButton.setText("获取当前目录");
+        getDirButton.setText("获取目录");
+        getDirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    sendPacket(3);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
         sendButton = new JButton();
         sendButton.setText("发送信息");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    sendPacket(4);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
         choosePanel.add(getTimeButton);
         choosePanel.add(getDateButton);
         choosePanel.add(getDirButton);
--- src/com/liz/Server.java
+++ src/com/liz/Server.java
@@ -4,17 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Server extends JFrame {
     private JPanel totalPanel;
-    private JPanel secPanel;
     private JPanel topPanel;
-    private JPanel bottomPanel;
     private JPanel portPanel;
     private JTextField portField;
     private JLabel portLabel;
@@ -22,6 +24,7 @@ public class Server extends JFrame {
     private JPanel receivePanel;
     private JEditorPane receiveEditorPane;
     private JLabel currLabel;
+    private DatagramSocket socket;
 
     Server() {
         super("Server");
@@ -37,28 +40,64 @@ public class Server extends JFrame {
 
     private class DatagramServer extends Thread {
         DatagramSocket socket;
-        DatagramPacket packet;
-        int client;
-        public DatagramServer(DatagramSocket socket, DatagramPacket packet, int client) {
+        public DatagramServer(DatagramSocket socket) {
             this.socket = socket;
-            this.packet = packet;
-            this.client = client;
             start();
         }
 
         public void run() {
-            try {
-                byte[] received = packet.getData();
-                receiveEditorPane.setText(receiveEditorPane.getText() + "Received from client= " + client + " " + new String(received));
-//                byte[] buf = received;
-//                InetAddress address = packet.getAddress();
-//                int port = packet.getPort();
-//                packet = new DatagramPacket(buf, buf.length, address, port);
-            } catch (Exception e) {
-                e.printStackTrace();
+            while(true) {
+                try {
+                    byte[] buf = new byte[256];
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    // 若socket已被关闭,则会抛出SocketException,跳出循环
+                    try {
+                        socket.receive(packet);
+                    } catch (SocketException e) {
+                        break;
+                    }
+                    String received = new String(packet.getData());
+                    String[] strings = received.split("\\|");
+                    receiveEditorPane.setText(receiveEditorPane.getText() + "Received from client= " + packet.getAddress().getHostName() + ":" + packet.getPort() + "\n");
+                    int type = Integer.parseInt(strings[0]);
+                    String contents = "";
+                    String notice = "";
+                    SimpleDateFormat df;
+                    switch (type) {
+                        case 1:
+                            df = new SimpleDateFormat("HH:mm:ss");
+                            contents += df.format(new Date());
+                            notice = "接收到获取时间请求";
+                            break;
+                        case 2:
+                            df = new SimpleDateFormat("yyyy-MM-dd");
+                            contents += df.format(new Date());
+                            notice = "接收到获取日期请求";
+                            break;
+                        case 3:
+                            File file = new File(strings[1]);
+                            File[] files = file.listFiles();
+                            for (File f : files) {
+                                contents += f.getName() + "\t";
+                            }
+                            notice = "接收到获取目录请求";
+                            break;
+                        case 4:
+                            contents += "服务器已接受到消息: " + strings[1];
+                            notice = "接收到信息";
+                            break;
+                    }
+                    receiveEditorPane.setText(receiveEditorPane.getText() + notice + ":" + contents + "\n");
+                    buf = contents.getBytes();
+                    InetAddress address = packet.getAddress();
+                    int port = packet.getPort();
+                    packet = new DatagramPacket(buf, buf.length, address, port);
+                    socket.send(packet);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
-
     }
 
     private void addPanel() {
@@ -78,34 +117,35 @@ public class Server extends JFrame {
         portPanel.add(portField);
         portButton = new JButton();
         portButton.setText("Start");
-//        portButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                if(portButton.getText().equals("Start")) {
-//                    if (!portField.getText().equals(String.valueOf(Integer.parseInt(portField.getText()))))
-//                        receiveEditorPane.setText(receiveEditorPane.getText() + "端口填写错误,请填写一个1~65535间的数字\n");
-//                    else {
-//                        byte[] buf = new byte[256];
-//                        try {
-//                            DatagramSocket socket = new DatagramSocket(Integer.parseInt(portField.getText()));
-//                            DatagramPacket packet = new DatagramPacket(buf, 256);
-//                            receiveEditorPane.setText(receiveEditorPane.getText() + "服务启动,端口为:" + portField.getText());
-//                            socket.receive(packet);
-//                        } catch (SocketException e1) {
-//                            receiveEditorPane.setText(receiveEditorPane.getText() + e1.toString() + " Error!!!\n");
-//                        } catch (IOException e1) {
-//                            receiveEditorPane.setText(receiveEditorPane.getText() + e1.toString() + " Error!!!\n");
-//                        }
-//
-//
-//                    }
-//                } else if (portButton.getText().equals("Stop")) {
-//
-//                } else {
-//                    receiveEditorPane.setText(receiveEditorPane.getText() + "Button Error!!!\n");
-//                }
-//            }
-//        });
+        portButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(portButton.getText().equals("Start")) {
+                    // 当端口不为数字时报错
+                    if (!portField.getText().equals(String.valueOf(Integer.parseInt(portField.getText()))))
+                        receiveEditorPane.setText(receiveEditorPane.getText() + "端口填写错误,请填写一个1~65535间的数字\n");
+                    else {
+                        // 正常开启服务
+                        portButton.setText("Stop");
+                        currLabel.setText("服务启动,端口为:" + portField.getText());
+                        try {
+                            socket = new DatagramSocket(Integer.parseInt(portField.getText()));
+                        } catch (SocketException e1) {
+                            e1.printStackTrace();
+                        }
+                        // 开启DatagramServer线程
+                        new DatagramServer(socket);
+                    }
+                } else if (portButton.getText().equals("Stop")) {
+                    // 关闭socket,会导致已开启的DatagramServer线程因获取到SocketException而停止
+                    socket.close();
+                    currLabel.setText("服务未启动");
+                    portButton.setText("Start");
+                } else {
+                    receiveEditorPane.setText(receiveEditorPane.getText() + "Button Error!!!\n");
+                }
+            }
+        });
         portPanel.add(portButton);
 
         receivePanel = new JPanel();
@@ -113,6 +153,7 @@ public class Server extends JFrame {
         receivePanel.setBorder(BorderFactory.createTitledBorder("日志"));
         receiveEditorPane = new JEditorPane();
         receiveEditorPane.setEditable(false);
+
         receivePanel.add(receiveEditorPane);
         topPanel.add(BorderLayout.CENTER,receivePanel);
 
