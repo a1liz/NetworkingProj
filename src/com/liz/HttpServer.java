30478073bfc455d5c06c4b44cb7765c18caddcda
a1liz <694426737@qq.com>
2017-11-16 13:47:47 +0800

    添加了log部分

--- src/com/liz/HttpServer.java
+++ src/com/liz/HttpServer.java
@@ -34,48 +34,91 @@ public class HttpServer extends Thread {
         public void run() {
             try {
                 String request = bufferedReader.readLine();
-                System.out.println("Request: " + request);
-                StringTokenizer stringTokenizer = new StringTokenizer(request);
-                if ((stringTokenizer.countTokens() >= 2) && stringTokenizer.nextToken().equals("GET")) {
-                    if ((request = stringTokenizer.nextToken()).startsWith("/"))
-                        request = request.substring(1);
-                    if (request.endsWith("/")||request.equals(""))
-                        request = request + "index.html";
-                    File f = new File("htdocs/" + request);
-                    showDoc(dataOutputStream,f);
-                } else {
-                    dataOutputStream.writeBytes("400 Bad Request");
+                System.out.println("Log: request: " + request);
+                if (request != "null");
+                {
+                    StringTokenizer stringTokenizer = new StringTokenizer(request);
+                    if ((stringTokenizer.countTokens() >= 2) && stringTokenizer.nextToken().equals("GET")) {
+                        if ((request = stringTokenizer.nextToken()).startsWith("/"))
+                            request = request.substring(1);
+                        if (request.endsWith("/") || request.equals(""))
+                            request = request + "index.html";
+                        showDoc(dataOutputStream, request);
+                    } else {
+                        dataOutputStream.writeBytes("400 Bad Request");
+                    }
+                    dataOutputStream.close();
+                    client.close();
                 }
-                dataOutputStream.close();
-                client.close();
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        public void showDoc(DataOutputStream out, File f) throws Exception {
+        private void showDoc(DataOutputStream out, String request) throws Exception {
             try {
+                File f = new File("htdocs/" + request);
+                String[] tmp = request.split("\\.");
+                String fileExtension = tmp[tmp.length - 1];
                 DataInputStream in = new DataInputStream(new FileInputStream(f));
                 int len = (int) f.length();
                 byte[] buf = new byte[len];
                 in.readFully(buf);
                 in.close();
-                out.writeBytes("HTTP/1.1 200 OK\r\n");
-                out.writeBytes("Content-Length: " + buf.length + " \r\n");
-                out.writeBytes("Content-Type: text/HTML \r\n\r\n");
+
+                String response = "HTTP/1.1 200 OK\r\n" + "Content-Length: " + buf.length + " \r\nContent-Type: " + getContentType(fileExtension) + "\r\n\r\n";
+                out.writeBytes(response);
                 out.write(buf);
+                System.out.println("Log: send " + response);
                 out.flush();
                 out.close();
             } catch (FileNotFoundException e) {
-                out.writeBytes("HTTP/1.1 404 "+"Not Found \r\n");
-                out.writeBytes("Content-Type text/HTML \r\n\r\n");
+                String response = "HTTP/1.1 404 Not Found \r\n" + "Content-Type text/HTML \r\n\r\n" + "<!DOCTYPE html><html><head><title> 404 NOT FOUND</title></head><body>404 not found!</body></html>";
+                out.writeBytes(response);
+                System.out.println("Log: send " + response);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
+        private String getContentType(String s) {
+            String type;
+            switch (s) {
+                case "html":
+                    type = "text/html;charset=UTF-8";
+                    break;
+                case "htm":
+                    type = "text/html;charset=UTF-8";
+                    break;
+                case "png":
+                    type = "image/png";
+                    break;
+                case "gif":
+                    type = "image/gif";
+                    break;
+                case "jpg":
+                    type = "image/jpeg";
+                    break;
+                case "jpeg":
+                    type = "image/jpeg";
+                    break;
+                case "css":
+                    type = "text/css";
+                    break;
+                case "js":
+                    type = "application/x-javascript";
+                    break;
+                case "bmp":
+                    type = "application/x-bmp";
+                    break;
+                default:
+                    type = "text/plain;charset=gbk";
+                    break;
+            }
+            return type;
+        }
     }
+
     public HttpServer() {
         try {
             listen = new ServerSocket(port);
